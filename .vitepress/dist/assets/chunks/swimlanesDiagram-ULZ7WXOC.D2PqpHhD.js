adc15ea1a9016c2af3e8dd2613e8e4633a048de4
zhangshuhan <anghunk@gmail.com>
2026-08-14 15:38:16 +0800

    Initial commit

---
+++ .vitepress/dist/assets/chunks/swimlanesDiagram-ULZ7WXOC.D2PqpHhD.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN._1eDrdeW.js";import{_ as a}from"../app._YtjgbNm.js";import"./chunk-5VM5RSS4.Cpm9DF9g.js";import"./chunk-XXDRQBXY.C7u1Q3-i.js";import"./chunk-KBJHAD2P.zNwgjhRI.js";import"./chunk-2GRJ4B5K.CP4IjQ9e.js";import"./channel.BiVremxG.js";import"./framework.CCXDMzJr.js";import"./theme.BUyAntFY.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
